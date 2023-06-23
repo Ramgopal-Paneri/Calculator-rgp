e000fd37ec8a04eecf67ae6ccfcb6e8565b0c292
Ramgopal Paneri <rampaneri111@gmail.com>
2023-06-23 20:06:53 +0530

    updated js

--- jsforcalci.js
+++ jsforcalci.js
@@ -9,11 +9,11 @@ buttons.forEach(element => {
         if (buttonText == 'X') {
             screen.value = screenValue;
         }
-        // if (buttonText == '√') {
-        //     buttonText = '√';
-        //     screenValue += buttonText;
-        //     screen.value = screenValue;
-        // }
+        if (buttonText == '√') {
+            buttonText = '√';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
         else if (buttonText == 'C/AC') {
             screenValue = "";
             screen.value = screenValue;
@@ -22,28 +22,23 @@ buttons.forEach(element => {
             if(screen.value == ''){
                 alert("enter any value to calculate ")
             }
-            if(screenValue.length == 2){
-                if(screenValue.indexOf('√')==0){
-                   const getElem = screenValue[screenValue.indexOf('√')+1]
-                const getSqValue = Math.sqrt(parseInt(getElem));   
-                screenValue.replace('√',null);
-                screenValue.replace(getElem.value,null)
-               screenValue = screenValue.concat('',getSqValue)
-                }
-            }
-            else{
-                    if(screenValue.indexOf('√')){
-                        console.log(screenValue.indexOf('√'))
-                        console.log(screenValue.indexOf('√')+1)
-                    }     
+//             else{
+//                     if(screenValue.indexOf('√')){
+//                         console.log(screenValue.indexOf('√'))
+//                         console.log(screenValue.indexOf('√')+1)
+                        
 // let rootElemNO = screenValue.indexOf('√')+1;
 // console.log(screenValue[rootElemNO])
 //           rootEleValue = Math.sqrt(parseInt(screenValue[rootElemNO]));
 //           console.log(rootEleValue)
-
-        // }
-
-            screen.value = eval(screenValue.replace('X', '*'));
+//           rootEleString = rootEleValue.toString();
+//           console.log(rootEleString)
+//           elem = '√'+screenValue[rootElemNO];
+//           console.log(elem)
+//           screenValue.replace('√'+${screenValue[rootElemNO]},rootEleString)
+//           console.log(screenValue)            
+//         }
+//             screen.value = eval(screenValue.replace('X', '*'));
         }
     
         else {
